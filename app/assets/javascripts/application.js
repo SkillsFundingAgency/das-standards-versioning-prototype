a397c5c8fc3102950ef52409910eace86eae65bb
chrishewison1983 <chris@chewitdesign.co.uk>
2020-08-20 15:54:56 +0100

    Update to the email preferences

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,9 +25,44 @@ $(document).ready(function () {
 
 function openTab(tabNumber) {
      var i;
-     var x = document.getElementsByClassName("tab-number");
+     var x = document.getElementsByClassName("tab-number"); 
      for (i = 0; i < x.length; i++) {
           x[i].style.display = "none";
      }
      document.getElementById(tabNumber).style.display = "block";
 }
+
+// Multiple emails
+function addAnotherEmail() {
+     event.preventDefault();
+     emailNumber++;
+     $("#emails").append(`
+          <div class="govuk-form-group">
+               <label class="govuk-label" for="updates-email[url${emailNumber}]">
+                    Additional email address
+               </label>
+               <input class="govuk-input govuk-input--width-20" id="updates-email[url${emailNumber}]" name="updates-email[url${emailNumber}]" type="text" aria-describedby="updates-email-hint" spellcheck="false">
+               <p class="govuk-body">
+                    <a class="govuk-link govuk-link--no-visited-state" data-remove="url${emailNumber}" href="#">remove</a>
+               </p>
+          </div>
+     `).on("click", "[data-remove]", function () {
+     if ($("#emails").data("email-count") === 1) {
+          event.preventDefault();
+          $(this).closest(".govuk-form-group").remove();
+     }
+     });
+}
+
+function removeEmail() {
+     event.preventDefault();
+     var removeId = $(this).data("remove");
+     $("#emails").append(`
+          <input id="updates-email[${removeId}]" name="updates-email[${removeId}]" type="hidden" value="" />`
+     )
+     $(this).closest(".govuk-form-group").remove();
+}
+
+var emailNumber = $("#emails").data("email-count");
+$("#addAnotherEmail").on("click", addAnotherEmail);
+$("[data-remove]").on("click", removeEmail);
