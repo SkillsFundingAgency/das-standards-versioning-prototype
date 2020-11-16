6a0abba0e46760514fece338a073bd222afb2570
chrishewison1983 <chris@chewitdesign.co.uk>
2020-11-16 13:43:40 +0000

    Update to the EPAO journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apprenticeship assessment service',
+  serviceName: 'Standard versioning',
 
   // Default port that prototype runs on
   port: '3000',
