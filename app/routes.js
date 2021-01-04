e2bdd83deace1a8b721358320c61e823937ef836
DarrenSilcox2020 <darren@darrensilcox.com>
2021-01-04 14:03:47 +0000

    Fixing bugs and routing problem on last 2 journeys sorting some spelling inconsistancies fine tuning ready for a demo

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,27 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+// Re-direct from multiple  radios showing version options
+router.post('/sprint-11/epao3/2-confirm-apprentice1', function (req, res) {
+
+    let chooseversionoption = req.session.data['chooseversionoption']
+
+    console.log(req.session.data['chooseversionoption'])
+
+    //Option 1
+    if (chooseversionoption === 'Version 1.0') {
+        res.redirect('/sprint-11/epao3/2-confirm-apprentice1')
+    }
+    //Option 2
+    else if (chooseversionoption === 'Version 1.1') {
+        res.redirect('/sprint-11/epao3/2-confirm-apprentice2')
+    }
+    //Option 3
+    else if (chooseversionoption === 'Version 1.2') {
+        res.redirect('/sprint-11/epao3/2-confirm-apprentice3')
+    }
+    //end
+})
+
 module.exports = router
--- app/views/sprint-11/routes.js
+++ app/views/sprint-11/routes.js
@@ -21,45 +21,5 @@ router.use('/research/provider-portal/manage-your-apprentices', (req, res, next)
 
 
 
-// Re-direct from multiple nested radios showing version options
-router.post('/sprint-11/epao3/2-confirm-apprentice1', function (req, res) {
-
-    let chooseversionoption = req.session.data['chooseversionoption']
-
-    console.log(req.session.data['chooseversionoption'])
-
-    //Option 1
-    if (chooseversionoption === 'Version 1.0') {
-        res.redirect('/sprint-11/epao3/2-confirm-apprentice1')
-    }
-    //Option 2
-    else if (chooseversionoption === 'Version 1.1') {
-        res.redirect('/sprint-11/epao3/2-confirm-apprentice2')
-    }
-    //Option 3
-    else if (chooseversionoption === 'Version 1.2') {
-        res.redirect('/sprint-11/epao3/2-confirm-apprentice3')
-    }
-    //end
-})
-
-
-// Profile 3 directions
-router.post('/booking/noti-contact', function (req, res) {
-
-    let direct = req.session.data['direct']
-
-    console.log(req.session.data['direct'])
-
-    //Option 1
-    if (direct === 'contact-candidate') {
-        res.redirect('/booking/noti-contact')
-    }
-    //Option 2
-    else if (direct === 'cancel-booking') {
-        res.redirect('/booking/noti-cancel')
-    }
-    //end
-})
 
 module.exports = router
