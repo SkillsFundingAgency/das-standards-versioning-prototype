5de2e537e6022f076f7be9370caff7ca1af2660c
victorivandfe <77013703+victorivandfe@users.noreply.github.com>
2021-02-09 22:13:03 +0000

    Changed these two EPAO record a grade journeys
    Changed the journeys so that when you select a certain version it will take you to a page that has options specific to that version.

--- app/routes.js
+++ app/routes.js
@@ -29,8 +29,70 @@ router.post('/sprint-11/epao3/2-confirm-apprentice1', function (req, res) {
 
 
 
+// Re-direct from multiple  radios showing version options
+router.post('/sprint-2-beta/epao7/2-options', function (req, res) {
+
+    let choosestandard = req.session.data['choosestandard']
+
+    console.log(req.session.data['choosestandard'])
+
+    //Option 1
+    if (choosestandard === 'Gas engineering operative') {
+        res.redirect('/sprint-2-beta/epao7/2-options')
+    }
+    //Option 2
+    else if (choosestandard === 'Gas network craftsperson') {
+        res.redirect('/sprint-2-beta/epao7/2-options1')
+    }
+
+    //end
+})
 
 
+
+
+
+
+// Re-direct from multiple  radios showing version options
+router.post('/sprint-2-beta/epao5/2a-options', function (req, res) {
+
+    let chooseversion = req.session.data['chooseversion']
+
+    console.log(req.session.data['chooseversion'])
+
+    //Option 1
+    if (chooseversion === 'Version 1.0') {
+        res.redirect('/sprint-2-beta/epao5/2a-options')
+    }
+    //Option 2
+    else if (chooseversion === 'Version 1.1') {
+        res.redirect('/sprint-2-beta/epao5/2b-options')
+    }
+
+    //end
+})
+
+
+
+
+// Re-direct from multiple  radios showing version options
+router.post('/sprint-1-beta/epao1/2a-options', function (req, res) {
+
+    let chooseversions = req.session.data['chooseversions']
+
+    console.log(req.session.data['chooseversions'])
+
+    //Option 1
+    if (chooseversions=== 'Version 1.0') {
+        res.redirect('/sprint-1-beta/epao1/2a-options')
+    }
+    //Option 2
+    else if (chooseversions === 'Version 1.1') {
+        res.redirect('/sprint-1-beta/epao1/2b-options')
+    }
+
+    //end
+})
 
 
 
